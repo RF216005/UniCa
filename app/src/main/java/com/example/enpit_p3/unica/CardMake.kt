862cff8c4a7635f0fa93309c56b2eefcb6e38cc5
RF216005 <F216005@ed.tusy.ac.jp>
2018-10-26 21:06:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/enpit_p3/unica/CardMake.kt
@@ -0,0 +1,12 @@
+package com.example.enpit_p3.unica
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CardMake : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_shop)
+    }
+}
