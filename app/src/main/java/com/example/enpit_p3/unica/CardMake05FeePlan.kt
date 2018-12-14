59955da97d913e9dad3a089aeaa136bee50560bc
RF216014 <F216014@ed.tusy.ac.jp>
2018-12-14 20:12:49 +0900

    店の登録画面途中

--- app/src/main/java/com/example/enpit_p3/unica/CardMake05FeePlan.kt
+++ app/src/main/java/com/example/enpit_p3/unica/CardMake05FeePlan.kt
@@ -1,6 +1,7 @@
 package com.example.enpit_p3.unica
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.widget.RadioButton
 import kotlinx.android.synthetic.main.activity_card_make05_fee_plan.*
@@ -12,11 +13,17 @@ class CardMake05FeePlan : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_make05_fee_plan)
 
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val shopFeePlan = pref.getString("SHOP_FEE_PLAN", "")
+
         back5.setOnClickListener {
             startActivity<CardMake04LayoutCheck>()
         }
 
         next5.setOnClickListener {
+            pref.edit()
+                    .putString("SHOP_FEE_PLAN", textView5.text.toString())
+                    .apply()
             startActivity<CardMake06LastResult>()
         }
 
